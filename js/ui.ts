3ae625b388fd75e2d4dea56c4d0fa7d57cf8003e
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-05-20 08:33:13 -0400

    Change

--- js/ui.ts
+++ js/ui.ts
@@ -20,17 +20,17 @@ const showBoard = () => {
 		line.forEach((_cell, j) => {
 			const buttonID = `${i}-${j}`;
 			const buttonClass = "item draggable-button";
+			const isCellValueEmpty = !board.getCellValue(i, j);
 
 			let buttonInnerHTML = "";
-			if (!board.getCellValue(i, j)) {
+			if (isCellValueEmpty) {
 				if (showCross()) {
 					board.markCell(i, j, true);
-
 					buttonInnerHTML = `
-					<button class="item">
-						<i class="board-cross fa-solid fa-xmark"></i>
-					</button>
-					`;
+            <button class="item">
+              <i class="board-cross fa-solid fa-xmark"></i>
+            </button>
+          `;
 				} else {
 					buttonInnerHTML = `<button id='${buttonID}' class='${buttonClass}'></button>`;
 				}
@@ -43,42 +43,43 @@ const showBoard = () => {
 	});
 };
 
+const showBlockCount = (blockNumberTotal: number[][], orientation: boolean) =>
+	blockNumberTotal
+		.map(
+			(blockCount) =>
+				`<p><span${orientation ? ' class="vertical"' : ""}>${blockCount.join(
+					`</span><span ${orientation ? ' class="vertical"' : ""}>`,
+				)}</span></p>`,
+		)
+		.join("");
+
 const showBlockCountVertical = () => {
 	if (!boardValuesVerticalHTML) return;
 	const blockNumberTotal = board.getBlockCountVertical();
-
-	boardValuesVerticalHTML.innerHTML = blockNumberTotal
-		.map(
-			(blockCount) => `<p><span>${blockCount.join("</span><span>")}</span></p>`,
-		)
-		.join("");
+	boardValuesVerticalHTML.innerHTML = showBlockCount(blockNumberTotal, false);
 };
 
 const showBlockCountHorizontal = () => {
 	if (!boardValuesHorizontalHTML) return;
 	const blockNumberTotal = board.getBlockCountHorizontal();
-
-	boardValuesHorizontalHTML.innerHTML = blockNumberTotal
-		.map(
-			(blockCount) =>
-				`<p><span class="vertical">${blockCount.join(
-					'</span><span class="vertical">',
-				)}</span></p>`,
-		)
-		.join("");
+	boardValuesHorizontalHTML.innerHTML = showBlockCount(blockNumberTotal, true);
 };
 
 const listenBoardButtons = () => {
 	if (!boardHTML) return;
 
+	let prevMouseX = 0;
+	let prevMouseY = 0;
 	let isDragging = false;
+	let rowBounds = { top: Infinity, bottom: Infinity };
+	let columnBounds = { left: Infinity, right: Infinity };
 	const boardButtons = document.querySelectorAll(".item");
 
-	boardHTML.addEventListener("mousedown", () => {
+	boardHTML.addEventListener("mousedown", (eventGlobal: MouseEvent) => {
 		isDragging = true;
 
 		boardButtons.forEach((button: HTMLButtonElement) => {
-			button.addEventListener("click", (event: Event) => {
+			button.addEventListener("click", (event) => {
 				event.preventDefault();
 				const target = event.target as HTMLElement;
 
@@ -88,22 +89,25 @@ const listenBoardButtons = () => {
 						const [row = 0, column = 0] = matchResult
 							.filter(Boolean)
 							.map(Number);
+
 						if (SQUARE_MODE && board.getCellValue(row, column)) {
 							board.markCell(row, column, true);
 							board.setMaskCellValue(row, column, true);
 							const cellHTML = document.getElementById(target.id);
 							if (!cellHTML) return;
 							cellHTML.outerHTML = `
-						<button class="item2"></button>
-						`;
+								<button class="item2"></button>
+							`;
 						} else if (!SQUARE_MODE && !board.getCellValue(row, column)) {
 							const cellHTML = document.getElementById(target.id);
 							if (!cellHTML) return;
 							cellHTML.outerHTML = `
-						<button class="item">
-							<i class="board-cross fa-solid fa-xmark"></i>
-						</button>
-						`;
+								<button class="item">
+								<i class="board-cross fa-solid fa-xmark"></i>
+								</button>
+							`;
+						} else {
+							isDragging = false;
 						}
 
 						const { vertical, horizontal } = board.checkLines(row, column);
@@ -111,32 +115,26 @@ const listenBoardButtons = () => {
 						if (vertical) {
 							for (let i = 0; i < board.size; i++) {
 								const cellHTML = document.getElementById(`${row}-${i}`);
-
-								if (cellHTML === null) continue;
-
+								if (!cellHTML) continue;
 								board.markCell(row, i, true);
-
 								cellHTML.outerHTML = `
-							<button class="item">
-								<i class="board-cross fa-solid fa-xmark"></i>
-							</button>
-							`;
+									<button class="item">
+									<i class="board-cross fa-solid fa-xmark"></i>
+									</button>
+								`;
 							}
 						}
 
 						if (horizontal) {
 							for (let i = 0; i < board.size; i++) {
 								const cellHTML = document.getElementById(`${i}-${column}`);
-
-								if (cellHTML === null) continue;
-
+								if (!cellHTML) continue;
 								board.markCell(i, column, true);
-
 								cellHTML.outerHTML = `
-							<button class="item">
-								<i class="board-cross fa-solid fa-xmark"></i>
-							</button>
-							`;
+									<button class="item">
+									<i class="board-cross fa-solid fa-xmark"></i>
+									</button>
+								`;
 							}
 						}
 					} else {
@@ -145,75 +143,94 @@ const listenBoardButtons = () => {
 				}
 			});
 
-			button.addEventListener("mousemove", (event) => {
-				//if (isDragging && isMouseOverButton(event, button)) {
-				if (isDragging && isMouseOverRowOrColumn(event, board.size)) {
+			button.addEventListener("mousemove", (eventLocal: MouseEvent) => {
+				const mouseX = eventGlobal.clientX;
+				const mouseY = eventGlobal.clientY;
+
+				if (Math.abs(mouseX - prevMouseX) >= 30) {
+					getRowAndColumnBounds(eventLocal, true);
+					prevMouseX = mouseX;
+				} else if (Math.abs(mouseY - prevMouseY) >= 30) {
+					getRowAndColumnBounds(eventLocal, false);
+					prevMouseY = mouseY;
+				}
+
+				if (isDragging && isMouseOverRowOrColumn(eventLocal)) {
 					button.click();
 				}
 			});
 		});
 	});
 
-	document.addEventListener("mouseup", () => {
+	document.addEventListener("mouseup", (e) => {
+		e.preventDefault();
 		isDragging = false;
+		boardHTML.removeEventListener("mousedown", () => {});
+		rowBounds = { top: Infinity, bottom: Infinity };
+		columnBounds = { left: Infinity, right: Infinity };
 	});
 
-	function isMouseOverRowOrColumn(event: MouseEvent, N: number) {
-		const fallbackValue = 10;
+	function getRowAndColumnBounds(event: MouseEvent, orientation: boolean) {
 		const matrixRect = boardHTML?.getBoundingClientRect();
-		const gap = 3;
-		const totalGapWidth = gap * (N - 1);
-		const buttonWidth =
-			(matrixRect?.width ?? fallbackValue - totalGapWidth) / N;
-		const buttonHeight =
-			(matrixRect?.height ?? fallbackValue - totalGapWidth) / N;
-		const mouseX = event.clientX - (matrixRect?.left ?? fallbackValue);
-		const mouseY = event.clientY - (matrixRect?.top ?? fallbackValue);
-		const row = Math.floor(mouseY / (buttonHeight + gap));
-		const col = Math.floor(mouseX / (buttonWidth + gap));
-		const rowBounds = {
-			top: row * (buttonHeight + gap),
-			bottom: (row + 1) * buttonHeight + row * gap,
-		};
-		const colBounds = {
-			left: col * (buttonWidth + gap),
-			right: (col + 1) * buttonWidth + col * gap,
-		};
+		if (
+			!matrixRect ||
+			matrixRect.left === undefined ||
+			matrixRect.top === undefined
+		) {
+			throw new Error("Failed to calculate border dimensions");
+		} else {
+			const gap = 3;
+			const buttonSize = 30;
+			const mouseX = event.clientX - matrixRect.left;
+			const mouseY = event.clientY - matrixRect.top;
+			const row = Math.floor(mouseY / (buttonSize + gap));
+			const col = Math.floor(mouseX / (buttonSize + gap));
 
-		// console.log(rowBounds, colBounds);
-
-		if (mouseX >= colBounds.left && mouseX <= colBounds.right) {
-			for (let i = 0; i < N; i++) {
-				const buttonTop = i * (buttonHeight + gap);
-				const buttonBottom = (i + 1) * buttonHeight + i * gap;
-				if (mouseY >= buttonTop && mouseY <= buttonBottom) {
-					return true;
-				}
+			if (orientation) {
+				rowBounds = {
+					top: row * (buttonSize + gap),
+					bottom: row * (buttonSize + gap) + 30,
+				};
+				columnBounds = {
+					left: 0,
+					right: matrixRect.right - matrixRect.left,
+				};
+			} else {
+				rowBounds = {
+					top: 0,
+					bottom: matrixRect.bottom - matrixRect.top,
+				};
+				columnBounds = {
+					left: col * (buttonSize + gap),
+					right: col * (buttonSize + gap) + 30,
+				};
 			}
-		} else if (mouseY >= rowBounds.top && mouseY <= rowBounds.bottom) {
-			for (let i = 0; i < N; i++) {
-				const buttonLeft = i * (buttonWidth + gap);
-				const buttonRight = (i + 1) * buttonWidth + i * gap;
-				if (mouseX >= buttonLeft && mouseX <= buttonRight) {
-					return true;
-				}
+
+			if (rowBounds.top === 0) {
+				console.log({ mouseX, mouseY, rowBounds, columnBounds });
 			}
 		}
-
-		return false;
 	}
 
-	/*function isMouseOverButton(event, button) {
-		const rect = button.getBoundingClientRect();
-		const mouseX = event.clientX;
-		const mouseY = event.clientY;
-		return (
-			mouseX >= rect.left &&
-			mouseX <= rect.right &&
-			mouseY >= rect.top &&
-			mouseY <= rect.bottom
-		);
-	}*/
+	function isMouseOverRowOrColumn(event: MouseEvent) {
+		const matrixRect = boardHTML?.getBoundingClientRect();
+		if (
+			!matrixRect ||
+			matrixRect.left === undefined ||
+			matrixRect.top === undefined
+		) {
+			throw new Error("Failed to calculate border dimensions");
+		} else {
+			const mouseX = event.clientX - matrixRect.left;
+			const mouseY = event.clientY - matrixRect.top;
+			return (
+				mouseX >= columnBounds.left &&
+				mouseX <= columnBounds.right &&
+				mouseY >= rowBounds.top &&
+				mouseY <= rowBounds.bottom
+			);
+		}
+	}
 };
 
 export {
